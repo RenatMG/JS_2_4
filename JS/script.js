dfaea3159131ba15b23ad115408cdece4e356a9d
RenatMG <kaverina2@yandex.ru>
2017-05-24 17:31:53 +0400

    17-31

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,22 @@
 'use strict';
 var str = 'A car, a Man, a maraca';
 
+//function isPalindrome(str) {
+//    str=(str.replace(/\W/g,"")).toUpperCase();
+//    var l=(str.length-1);
+//    for(var i=0;i<l/2;i++){
+//        if(str.charAt(i)!==(str.charAt(l-i))){
+//            return false;
+//        }
+//        if(i===l/2-1){
+//            return true;
+//        }
+//    }
+//}
 function isPalindrome(str) {
-    str=str.toUpperCase();
-    str=str.replace(/\W/g,"");
-    var l=(str.length-1);
-    for(var i=0;i<l/2;i++){
-        if(str.charAt(i)!==(str.charAt(l-i))){
-            return false;
-        }
-        if(i===l/2-1){
-            return true;
-        }
-    }
+    var a=(str.replace(/\W+/g,"")).split('');
+    return a.toString()===(a.reverse()).toString();
 }
+
 
 console.log(isPalindrome(str));

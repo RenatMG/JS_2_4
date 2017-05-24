f8566fbb35eb9dbc222c690388b5a21ef626f599
RenatMG <kaverina2@yandex.ru>
2017-05-24 16:39:31 +0400

    16-39

--- JS/script.js
+++ JS/script.js
@@ -2,14 +2,14 @@
 var str = 'A car, a Man, a maraca';
 
 function isPalindrome(str) {
-    str = str.toUpperCase();
-    str = str.replace(/\W/g, "");
-    var l = (str.length - 1);
-    for (var i = 0; i < l / 2; i++) {
-        if (str.charAt(i) !== (str.charAt(l - i))) {
+    str=str.toUpperCase();
+    str=str.replace(/\W/g,"");
+    var l=(str.length-1);
+    for(var i=0;i<l/2;i++){
+        if(str.charAt(i)!==(str.charAt(l-i))){
             return false;
         }
-        if (i === l / 2 - 1) {
+        if(i===l/2-1){
             return true;
         }
     }

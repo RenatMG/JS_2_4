c684819b0d97a824bd7d14677a7fed9423857ac3
RenatMG <kaverina2@yandex.ru>
2017-05-24 16:17:14 +0400

    16-17

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,18 @@
 'use strict';
+var str = 'A car, a Man, a maraca';
+
+function isPalindrome(str) {
+    str = str.toUpperCase();
+    str = str.replace(/\W/g, "");
+    var l = (str.length - 1);
+    for (var i = 0; i < l / 2; i++) {
+        if (str.charAt(i) !== (str.charAt(l - i))) {
+            return false;
+        }
+        if (i === l / 2 - 1) {
+            return true;
+        }
+    }
+}
+
+console.log(isPalindrome(str));
